c10a062dd8b66cf74d8cc016e37804f30f4f2808
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-09-29 19:24:27 +0530

    first commit

---
+++ variables.py
@@ -0,0 +1,26 @@
+# Variables
+
+# x= 1    # init
+# y = 2.5 # float
+# name = 'John' #String
+# is_cool = True #bool
+
+# multiple Assignment
+x,y,name,is_cool = (1,2.5,'john',True)
+
+# print('Hello')
+# print(x,y,name,is_cool)
+
+# Basic math
+a = x + y
+
+#Casting
+x = str(x)
+y = int(x)
+z = float(x)
+
+# Type
+print(type(x))
+
+
+
