0ee8a9c5d65fc2eddcf9ca4c6eb68037a9c8c3da
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-09-29 21:12:20 +0530

    tuples& sets

---
+++ tuples_sets.py
@@ -0,0 +1,43 @@
+# Turples
+
+# fruits = ('Apples','Oranges','Grapes')
+# fruits2 = tuple(('Apple','Oranges','Grapes'))
+
+# get value needs trailing comma
+# fruits2 = ('Apple',)
+
+# get Value
+# print(fruits[0])
+
+# Delete tuple
+# del fruits2
+
+# Get length
+# print(len(fruits))
+
+# print(fruits , type(fruits2))
+
+
+# Sets
+
+# Create Set
+fruits_set = {'Apple','Orange','Pears'}
+
+# check if is set
+print('Apple' in fruits_set)
+
+# Add to set
+fruits_set.add('Grape')
+
+
+# Add to duplicate
+fruits_set.add('Grape')
+
+# Clear Set
+fruits_set.clear()
+
+# Delete
+del fruits_set
+
+print(fruits_set)
+
