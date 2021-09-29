f36bacb346a4c8bd49d28630331557edcc0d21f3
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-09-29 20:38:14 +0530

    lists

---
+++ lists.py
@@ -0,0 +1,42 @@
+#Lists
+
+# Create Lists
+numbers = [1,2,3,4,5,6]
+fruits = ['Apples','Oranges','Grapes','Pears']
+
+# Use a Constructor
+numbers2 = list((1,2,3,4,5,6))
+
+# print(numbers ,numbers2)
+
+# Get a Value
+# print(fruits[1])
+
+# get a length
+# print(len(fruits))
+
+# Append List
+# fruits.append('Mongoes')
+
+#Remove from list
+# fruits.remove('Grapes')
+
+#Insert into position
+# fruits.insert(2,'Strawberries')
+
+# Change value
+fruits[0] = 'Blueberries'
+
+# Remove with pop
+# fruits.pop(2)
+
+#Reverse List
+# fruits.reverse()
+
+#Sort list
+# fruits.sort()
+
+#reverse sort
+fruits.sort(reverse=True)
+
+print(fruits)
