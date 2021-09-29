a5c54775e75e586ca6c34ee71ea3557c559b328f
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-09-29 20:21:25 +0530

    strings

---
+++ strings.py
@@ -0,0 +1,58 @@
+# Strings
+
+name = 'Gimna'
+age = 21
+
+# Concatenate
+# print('Hello, my name is ' + name + ' and I am '+ str(age) + ' years Old')
+
+#Arguments by positions
+# print('My name is {name} and I am {age}'.format(name=name , age=age))
+
+# F-String
+# print(f'Hello my name {name} and I am {age}')
+
+s = 'hello world'
+
+#capitalize
+# print(s.capitalize())
+
+# Make all uppercase
+# print(s.upper())
+
+# Make all lowercase
+# print(s.lower())
+
+#Swap case
+# print(s.swapcase())
+
+#Get length
+# print(len(s))
+
+# Replace
+# print(s.replace('world','everyone'))
+
+# Count
+# sub = 'h'
+# print(s.count(sub))
+
+#starts with
+# print(s.startswith('hello'))
+
+#End with
+# print(s.endswith('d'))
+
+# Split into an array
+# print(s.split())
+
+# Find a position
+# print(s.find('r'))
+
+# Is all aplanumeric
+# print(s.isalnum())
+
+# Is all alphebatic
+# print(s.isalpha())
+
+# Is all numeric
+print(s.isnumeric())
